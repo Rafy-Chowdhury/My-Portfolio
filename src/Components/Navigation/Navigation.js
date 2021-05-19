8c32573a7dff15048bec087493badca63cd7fb7d
Rafy Chowdhury <rafichowdhury115@gmail.com>
2021-05-19 14:48:17 +0600

    navbar edited

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,19 +4,23 @@ import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <div>
-            
-            <Navbar bg="dark" expand="lg">
-  
+
+      <div style={{backgroundColor: 'crimson'}}>
+        <div className="container">          
+  <Navbar expand="lg">
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
       <Nav.Link style={{color: 'white'}} href="#home">Home</Nav.Link>
-      <Nav.Link style={{color: 'white'}} href="#footer">Link</Nav.Link>
+      <Nav.Link style={{color: 'white'}} href="#footer">Blog</Nav.Link>
+      <Nav.Link style={{color: 'white'}} href="#footer">About</Nav.Link>
+      <Nav.Link style={{color: 'white'}} href="#footer">Contact</Nav.Link>
     </Nav>
   </Navbar.Collapse>
-</Navbar>
-        </div>
+ </Navbar>
+  </div>
+      </div>
+
     );
 };
 
